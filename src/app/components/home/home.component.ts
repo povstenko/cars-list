d478dbb4babce48cb40f9b78600e80c312bc904b
Vitaliy Povstenko <vitaly.povstenko@gmail.com>
2021-04-01 10:44:57 +0300

    home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Car } from 'src/app/models/Car';
+import { CarsService } from 'src/app/services/cars.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private carsService: CarsService, private router: Router) { }
+  cars: Car[] = [];
 
   ngOnInit(): void {
+    this.carsService.getCars().subscribe((data) => {
+      this.cars = data;
+    })
   }
 
 }

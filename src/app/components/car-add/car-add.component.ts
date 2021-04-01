6bbf880d75e2dc235b5fbb9367c24463f0d2455b
Vitaliy Povstenko <vitaly.povstenko@gmail.com>
2021-04-01 23:32:45 +0300

    Change frontend

--- src/app/components/car-add/car-add.component.ts
+++ src/app/components/car-add/car-add.component.ts
@@ -1,5 +1,5 @@
-import { Location } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT, Location } from '@angular/common';
+import { Component, Inject, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CarsService } from 'src/app/services/cars.service';
@@ -11,6 +11,7 @@ import { CarsService } from 'src/app/services/cars.service';
 })
 export class CarAddComponent implements OnInit {
   constructor(
+    @Inject(DOCUMENT) private _document: Document,
     private carsService: CarsService,
     private location: Location,
     private router: Router
@@ -29,7 +30,7 @@ export class CarAddComponent implements OnInit {
     if (myForm.valid) {
       this.carsService.addCar(name, model, price).subscribe(
         () => {
-          this.router.navigate(['/auth/maintenance']);
+          this._document.defaultView.location.reload();
         },
         (error) => {
           console.log(error);
